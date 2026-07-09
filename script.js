6980a86710e46765cbeafbd853a259e15c2ba2e5
hrittik702 <mmhrittik@gmail.com>
2026-07-09 23:48:58 +0530

    Optimise : Game Begin()

--- script.js
+++ script.js
@@ -2,39 +2,9 @@
 // winner = 1 (you)
 // winner = -1 (computer win)
 
-let your_score = 0;
+let user_score = 0;
 let comp_score = 0;
 let rounds = 0;
-
-let win_ratio = () => {
-  if (your_score === 0 && computer_score === 0) return 0;
-  else if (computer_score === 0) return your_score;
-  else return (your_score / computer_score).toFixed(1);
-};
-
-// Query Selectors
-const you_scr = document.querySelector("#you p");
-const cmp_scr = document.querySelector("#computer p");
-const status = document.querySelector(".status p:nth-child(2)");
-const round = document.querySelector(".status>p span");
-const winRatio = document.querySelector("nav button span");
-
-const game_begins = (num) => {
-  round.innerHTML = `${++rounds}`;
-  if (num == 0) {
-    status.innerHTML = "Match Draw!";
-    status.style.backgroundColor = "#e6e6fa";
-  } else if (num == 1) {
-    you_scr.innerHTML = `${++your_score}`;
-    status.innerHTML = "You Win!";
-    status.style.backgroundColor = "#3ae469";
-  } else if (num == -1) {
-    cmp_scr.innerHTML = `${++computer_score}`;
-    status.innerHTML = "Computer Wins!";
-    status.style.backgroundColor = "#ff5438";
-  }
-  winRatio.innerHTML = `${win_ratio()}`;
-};
 
 // Computer choice
 const getChoice = () => {
@@ -43,27 +13,55 @@ const getChoice = () => {
   return choices[choice_idx];
 };
 
-// game-logic
-const ans = (user_Choice, comp_Choice) => {
-  if (user_Choice === comp_Choice) return 0;
-  let userWin = 0;
-  if (user_Choice === "rock") {
-    userWin = comp_Choice === "paper" ? -1 : 1;
-  } else if (user_Choice === "paper") {
-    userWin = comp_Choice === "scissor" ? -1 : 1;
-  } else if (user_Choice === "scissor") {
-    userWin = comp_Choice === "rock" ? -1 : 1;
-  }
-  console.log(userWin);
-};
-
-// Your Choice
+// Your Choice & Game On
 let options = document.querySelectorAll("main div");
 options.forEach((choice) => {
   choice.addEventListener("click", () => {
     const user_Choice = choice.getAttribute("id");
     const comp_Choice = getChoice();
-    console.log(user_Choice, comp_Choice);
-    ans(user_Choice, comp_Choice);
+    game_begins(ans(user_Choice, comp_Choice), user_Choice, comp_Choice);
   });
 });
+
+// Game-logic
+const ans = (user_Choice, comp_Choice) => {
+  if (user_Choice === comp_Choice) return 0;
+  if (user_Choice === "rock" && comp_Choice === "scissor") return 1;
+  else if (user_Choice === "paper" && comp_Choice === "rock") return 1;
+  else if (user_Choice === "scissor" && comp_Choice === "paper") return 1;
+};
+
+let win_ratio = () => {
+  if (user_score === 0 && comp_score === 0) return 0;
+  else if (comp_score === 0) return user_score;
+  else return (user_score / comp_score).toFixed(1);
+};
+
+// Query Selectors
+const user_scr = document.querySelector("#you p");
+const cmp_scr = document.querySelector("#computer p");
+const status = document.querySelector(".status p:nth-child(2)");
+const round = document.querySelector(".status>p span");
+const winRatio = document.querySelector("nav button span");
+
+const game_begins = (num, user_Choice, comp_Choice) => {
+  round.innerHTML = `${++rounds}`;
+  if (num == 0) {
+    status.innerHTML = "It's draw. Try again !";
+    status.style.backgroundColor = "#012552";
+    status.style.color = "white";
+
+  } else if (num == 1) {
+    user_scr.innerHTML = `${++user_score}`;
+    status.innerHTML = `You Win! Your's ${user_Choice} beats ${comp_Choice}`;
+    status.style.backgroundColor = "green";
+    status.style.color = "white";
+
+  } else {
+    cmp_scr.innerHTML = `${++comp_score}`;
+    status.innerHTML = `You Lost!  ${comp_Choice} beats Your's ${user_Choice}`;
+    status.style.backgroundColor = "red";
+    status.style.color = "white";
+  }
+  winRatio.innerHTML = `${win_ratio()}`;
+};
